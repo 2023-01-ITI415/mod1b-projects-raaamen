98d332a52ffc094912e0a2991e58f496b480eea3
sarahPC <sarahapplebaum.sia@gmail.com>
2023-02-03 20:47:46 -0500

    built castle
    built a castle and made some scripts

--- Assets/02-Mission Demolition/Scripts/CloudCover.cs
+++ Assets/02-Mission Demolition/Scripts/CloudCover.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RigidbodySleep : MonoBehaviour
+public class CloudCover : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
