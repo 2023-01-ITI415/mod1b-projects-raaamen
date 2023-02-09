5c980d91d980f77f775c20936f37a342addbce84
sarah <sarahapplebaum.sia@gmail.com>
2023-02-09 18:48:30 -0500

    follow cam
    some stuff on followcam. progress is slow

--- Assets/02-Mission Demolition/Scripts/FollowCam.cs
+++ Assets/02-Mission Demolition/Scripts/FollowCam.cs
@@ -5,16 +5,24 @@ using UnityEngine;
 public class FollowCam : MonoBehaviour
 {
     public static GameObject projectile;
+    float camZ;
+    public Vector3 minimum = Vector3.zero;
 
     private void Awake() {
-        
+        camZ = transform.position.z;
     }
     
 
     private void FixedUpdate() {
         if (projectile=null) return;
-        Vector3.Lerp(transform.position, projectile.transform.position, Time
-        .deltaTime);
+        
+        Vector3 camDestination = projectile.transform.position;
+
+        
+
+        camDestination = Vector3.Lerp(transform.position, camDestination, 0.05f); 
+        camDestination.z = camZ;
+        transform.position=camDestination;
     }
 
 }
